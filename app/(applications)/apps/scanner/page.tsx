1b4130b91548963c8c3309569e9e01c112139f74
ajaxecho3 <ajaxecho3@gmail.com>
2025-01-12 22:04:13 +0800

    fix: build errro

--- app/(applications)/apps/scanner/page.tsx
+++ app/(applications)/apps/scanner/page.tsx
@@ -1,21 +1,11 @@
 "use client";
 
 import React from "react";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 function Scanner() {
-  const [data, setData] = React.useState("Not Found");
   return (
     <div>
-      <BarcodeScannerComponent
-        width={"100%"}
-        height={"100%"}
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
-        }}
-      />
-      <p>{data}</p>
+      <p>Scanner</p>
     </div>
   );
 }
